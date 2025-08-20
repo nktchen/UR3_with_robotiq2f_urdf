3ea43ca2eb21bba9cdc68bdd75999ef71adb2c8c
nktchen <chennikita70@gmail.com>
2025-08-20 14:08:29 +0300

    fixed setup.py

--- setup.py
+++ setup.py
@@ -4,26 +4,26 @@ import os
 package_name = 'skoltech'
 this_dir = os.path.abspath(os.path.dirname(__file__))
 
-# always install package.xml
 data_files = [
-    (os.path.join('share', package_name), [os.path.join(this_dir, 'package.xml')]),
+    (os.path.join('share', package_name), ['package.xml']),
 ]
 
-# папки которые хотим рекурсивно установить
 dirs_to_install = ['launch', 'rviz', 'urdf', 'meshes', 'resource']
 
 def add_dir(base_dir):
-    path = os.path.join(this_dir, base_dir)
-    if not os.path.isdir(path):
+    abs_base = os.path.join(this_dir, base_dir)
+    if not os.path.isdir(abs_base):
         return
-    for root, _, files in os.walk(path):
+    for root, _, files in os.walk(abs_base):
         files = [f for f in files if not f.startswith('.')]
         if not files:
             continue
-        # относительный путь внутри пакета, например 'meshes/ur3/visual'
         rel_dir = os.path.relpath(root, this_dir)
         target_dir = os.path.join('share', package_name, rel_dir)
-        file_paths = [os.path.join(root, f) for f in files]
+        if rel_dir == '.':
+            file_paths = [f for f in files]  
+        else:
+            file_paths = [os.path.join(rel_dir, f) for f in files]
         data_files.append((target_dir, file_paths))
 
 for d in dirs_to_install:
