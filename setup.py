8274a627feb999c25bf6b4b0457ae644f0b575ed
nktchen <chennikita70@gmail.com>
2025-08-20 14:06:10 +0300

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,42 +1,44 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
 
 package_name = 'skoltech'
+this_dir = os.path.abspath(os.path.dirname(__file__))
 
-# соберём data_files: package.xml + рекурсивно все файлы из meshes, rviz и urdf
+# always install package.xml
 data_files = [
-    ('share/' + package_name, ['package.xml']),
+    (os.path.join('share', package_name), [os.path.join(this_dir, 'package.xml')]),
 ]
 
-def add_dir_to_data_files(base_dir):
-    """Добавляет все файлы из base_dir в data_files в виде
-       ('share/skoltech/<rel_dir>', [list_of_files])"""
-    if not os.path.isdir(base_dir):
+# папки которые хотим рекурсивно установить
+dirs_to_install = ['launch', 'rviz', 'urdf', 'meshes', 'resource']
+
+def add_dir(base_dir):
+    path = os.path.join(this_dir, base_dir)
+    if not os.path.isdir(path):
         return
-    for root, dirs, files in os.walk(base_dir):
+    for root, _, files in os.walk(path):
         files = [f for f in files if not f.startswith('.')]
         if not files:
             continue
-        rel_dir = os.path.relpath(root, '.')  # 'meshes/...' или 'rviz' и т.д.
+        # относительный путь внутри пакета, например 'meshes/ur3/visual'
+        rel_dir = os.path.relpath(root, this_dir)
         target_dir = os.path.join('share', package_name, rel_dir)
         file_paths = [os.path.join(root, f) for f in files]
         data_files.append((target_dir, file_paths))
 
-# добавить папки, если они есть
-add_dir_to_data_files('meshes')
-add_dir_to_data_files('rviz')
-add_dir_to_data_files('urdf')
+for d in dirs_to_install:
+    add_dir(d)
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='nktchen',
     maintainer_email='nktchen@todo.todo',
-    description='skoltech package',
+    description='skoltech: URDF + meshes + launch + rviz',
     license='TODO',
     tests_require=['pytest'],
     entry_points={
